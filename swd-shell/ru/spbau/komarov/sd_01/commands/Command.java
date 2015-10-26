a0dc8d652306a1e86fb31b884b1a69c1693cf7c5
AYaKomarov <a.ya.komarov@gmail.com>
2015-10-27 00:23:50 +0300

    delete old version swd-shell, now look: SofwareDesign_classwork_01

--- swd-shell/ru/spbau/komarov/sd_01/commands/Command.java
+++
@@ -1,9 +0,0 @@
-package ru.spbau.komarov.sd_01.commands;
-
-import java.io.InputStream;
-import java.io.PrintStream;
-
-public interface Command {
-    public void execute(String arg, InputStream in, PrintStream out);
-    public String getInfo();
-}
--- swd-shell/ru/spbau/komarov/sd_01/commands/MetaCommand.java
+++
@@ -1,7 +0,0 @@
-package ru.spbau.komarov.sd_01.commands;
-
-import java.util.Map;
-
-abstract public class MetaCommand implements Command{
-    abstract public void setCommandMap(Map<String, Command> map);
-}
